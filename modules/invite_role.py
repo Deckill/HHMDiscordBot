6d7494405af8e35b8ac606061c1aa02f2b147ab2
Deckill <35498686+Deckill@users.noreply.github.com>
2025-06-01 10:47:49 +0900

    GCP3

--- modules/invite_role.py
+++ modules/invite_role.py
@@ -27,11 +27,14 @@ def setup(bot):
             if invite.code in old_invites and invite.uses > old_invites[invite.code]:
                 used_code = invite.code
                 break
-
+        
         invite_cache[guild.id] = {invite.code: invite.uses for invite in new_invites}
 
         if used_code and used_code in invite_code_to_role:
             role = discord.utils.get(guild.roles, name=invite_code_to_role[used_code])
             if role and member.guild.me.top_role > role:
                 await member.add_roles(role)
-                print(f"{member} → {role.name} 역할 부여")
+        print("새 멤버 입장:", member.name)
+        print("초기 invite_cache:", old_invites)
+        print("새 invite 목록:", {i.code: i.uses for i in new_invites})
+        print("사용된 초대 코드:", used_code)
