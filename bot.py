1b450931b26278368dbd61691f5b91c36cd60924
Deckill <35498686+Deckill@users.noreply.github.com>
2025-06-01 19:23:57 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@
 import os
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
-
+load_dotenv()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
